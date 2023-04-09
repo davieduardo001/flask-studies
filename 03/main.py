5f92fa2e6ada22ee7f8002c86eaceca346a7fa1d
davieduardo001 <ed.miranda@gmail.com>
2023-04-09 15:53:58 -0400

    bootstrap , access variables of python on html...

---
+++ 03/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    return render_template('market.html', item_name='phone')
